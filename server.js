78f8c845d55d5aa93c77d557d3ed78d34ed4c0d4
Chab Phanith <chabphanith2822@gmail.com>
2026-07-08 18:58:26 +0700

    fix: updated version

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express"; // Changed from 'require'
 import "dotenv/config";
 import cors from "cors";
+const cors = require('cors');
 import connectDB from "./configs/db.js";
 import userRouter from "./routes/userRoutes.js";
 import ownerRouter from "./routes/ownerRoutes.js";
@@ -15,6 +16,11 @@ await connectDB()
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+app.use(cors({
+    origin: 'https://car-rental-frontend-tan.vercel.app',
+    credentials: true
+  }));
 
 app.get('/', (req, res)=> res.send("Server is running"))
 app.use('/api/user', userRouter)
